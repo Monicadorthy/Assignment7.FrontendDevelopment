614948de2117ce6581532baff460f3669c328c92
Monicadorthy <monicadorthy@gmail.com>
2017-02-07 15:51:34 +0530

    seventh GitHub Commit
    seventh GitHub Commit

---
+++ app.js
@@ -0,0 +1,10 @@
+       //Javascript Function To Calculate Area of Circle
+       
+       function Calculatearea(){
+       //To get radius value
+       var radius=document.getElementById("radius").value;
+       //To Calculate the Area of Circle i.e Area=pi*r*r where pi=3.1415926535
+       var area=(3.14*radius*radius);
+       //To get Output in Browser Console
+       console.log("Area of circle is:"+area);
+       };
